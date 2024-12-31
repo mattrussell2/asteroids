55eb4ad5f1259605d194150c1c50d335e35625ac
Matt Russell <mrussell@cs.tufts.edu>
2024-12-31 00:41:57 -0500

    first commit

--- main.py
+++ main.py
@@ -1,21 +1,57 @@
 import pygame
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
+
     pygame.init()
+
+    updateable = pygame.sprite.Group()
+    drawables  = pygame.sprite.Group()
+    asteroids  = pygame.sprite.Group()
+    shots      = pygame.sprite.Group()
+
+    Player.containers = updateable, drawables
+    Asteroid.containers = asteroids, updateable, drawables
+    AsteroidField.containers = updateable
+    Shot.containers = shots, updateable, drawables
+
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    asteroid_field = AsteroidField()
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill((0, 0, 0))
-        pygame.display.flip()
+        for obj in updateable:
+            obj.update(dt)
 
-    # print("Starting asteroids!")
-    # print(f"Screen width: {SCREEN_WIDTH}")
-    # print(f"Screen height: {SCREEN_HEIGHT}") 
+        for asteroid in asteroids:
+            if player.check_collisions(asteroid):
+                print("Game over!")
+                return
+
+        for asteroid in asteroids:
+            for shot in shots:
+                if asteroid.check_collisions(shot):
+                    asteroid.split()
+                    shot.kill()
+
+        for obj in drawables:
+            obj.draw(screen)
+
+        dt = clock.tick(60) / 1000
+        pygame.display.flip()
+        
 
 if __name__ == '__main__':
     main()
---
+++ shot.py
@@ -0,0 +1,16 @@
+from circleshape import CircleShape
+from constants import *
+import pygame
+
+class Shot(CircleShape):
+    def __init__(self, x, y, velocity):
+        super().__init__(x, y, SHOT_RADIUS)
+        self.velocity = velocity
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, (255, 255, 255), self.position, self.radius)
+
+    def update(self, dt):
+        self.position += self.velocity * dt
+        if self.position.x < 0 or self.position.x > SCREEN_WIDTH or self.position.y < 0 or self.position.y > SCREEN_HEIGHT:
+            self.kill()
